86c83bbd15b66158a9b07fd40ab9a0fd45d2ccd2
Stephen Li <stephenli@google.com>
2016-06-29 17:36:05 -0700

    Working prototype with tests.
    When the plugin detects a patchset has been created, it will
    merge downstream until it hits a merge conflict. On the
    conflicting merge, it will vote -1 on a configurable label
    and provide instructions to resolving the merge conflict.
    
    Draft changes will be ignored until published.
    
    The plugin will put all the auto-created changes in the
    same topic as the original change (or create a topic if
    none exists). If a user updates the topic, it will update
    the topic of all the downstream merges.
    
    If there are existing downstream merges from a previous
    automerged patchset, it will update them all.
    
    Admins can update the config.yaml file in tools/automerger
    (this is configurable) in order to add merge paths and
    do other configuration changes.
    
    Javascript portion will add a button to optionally skip
    or merge all downstream changes. It will also bold all
    related changes in the same branch.
    
    Change-Id: I89209ed29073f987a37d03e10dad85a1c5c530fb

---
+++ src/main/java/com/googlesource/gerrit/plugins/automerger/FailedMergeException.java
@@ -0,0 +1,58 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.googlesource.gerrit.plugins.automerger;
+
+import com.google.common.base.Joiner;
+
+import java.util.Map;
+
+class FailedMergeException extends Exception {
+  private static final int MAX_CONFLICT_MESSAGE_LENGTH = 10000;
+
+  public final Map<String, String> failedMerges;
+
+  FailedMergeException(Map<String, String> failedMerges) {
+    this.failedMerges = failedMerges;
+  }
+
+  public String displayConflicts() {
+    StringBuilder output = new StringBuilder();
+    output.append("Merge conflict found on ");
+    output.append(failedMergeKeys());
+    output.append(". Please follow instructions at go/resolveconflict ");
+    output.append("to resolve this merge conflict.\n\n");
+
+    for (Map.Entry<String, String> entry : failedMerges.entrySet()) {
+      String branch = entry.getKey();
+      String message = entry.getValue();
+      String conflictMessage = message;
+      boolean truncated = false;
+      if (message.length() > MAX_CONFLICT_MESSAGE_LENGTH) {
+        conflictMessage = message.substring(0, MAX_CONFLICT_MESSAGE_LENGTH);
+        truncated = true;
+      }
+      output.append(branch);
+      output.append(":\n");
+      output.append(conflictMessage);
+      if (truncated) {
+        output.append("...\n\n");
+      }
+    }
+    return output.toString();
+  }
+
+  public String failedMergeKeys() {
+    return Joiner.on(", ").join(failedMerges.keySet());
+  }
+}
---
+++ src/test/java/com/googlesource/gerrit/plugins/automerger/ManifestReaderTest.java
@@ -0,0 +1,56 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.automerger;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.CharStreams;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.google.common.truth.Truth.assertThat;
+
+public class ManifestReaderTest {
+  private ManifestReader manifestReader;
+  private String manifestString;
+
+  @Before
+  public void setUp() throws Exception {
+    try (InputStream in = getClass().getResourceAsStream("default.xml")) {
+      manifestString = CharStreams.toString(new InputStreamReader(in, Charsets.UTF_8));
+    }
+    manifestReader = new ManifestReader("master", manifestString);
+  }
+
+  @Test
+  public void basicParseTest() throws Exception {
+    Set<String> expectedSet = new HashSet<String>();
+    expectedSet.add("platform/whee");
+    expectedSet.add("whoo");
+    assertThat(manifestReader.getProjects()).isEqualTo(expectedSet);
+  }
+
+  @Test
+  public void branchDifferentFromDefaultRevisionTest() throws Exception {
+    ManifestReader aospManifestReader = new ManifestReader("mirror-aosp-master", manifestString);
+    Set<String> expectedSet = new HashSet<String>();
+    expectedSet.add("platform/whaa");
+    assertThat(aospManifestReader.getProjects()).isEqualTo(expectedSet);
+  }
+}
