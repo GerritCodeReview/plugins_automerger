9e6bbf5b43339b5291d13acb834400c5c8d377ed
Stephen Li <stephenli@google.com>
2016-11-09 13:34:33 -0800

    Remove dependency on HttpPluginModule in favor of AbstractModule.
    Change-Id: I59be6aae2ba2f6782fc3076c56fb27cf33084263

--- src/main/java/com/googlesource/gerrit/plugins/automerger/HttpModule.java
+++ src/main/java/com/googlesource/gerrit/plugins/automerger/HttpModule.java
@@ -16,11 +16,11 @@ package com.googlesource.gerrit.plugins.automerger;
 import com.google.gerrit.extensions.registration.DynamicSet;
 import com.google.gerrit.extensions.webui.JavaScriptPlugin;
 import com.google.gerrit.extensions.webui.WebUiPlugin;
-import com.google.gerrit.httpd.plugins.HttpPluginModule;
+import com.google.inject.AbstractModule;
 
-public class HttpModule extends HttpPluginModule {
+public class HttpModule extends AbstractModule {
   @Override
-  protected void configureServlets() {
+  protected void configure() {
     DynamicSet.bind(binder(), WebUiPlugin.class).toInstance(new JavaScriptPlugin("automerger.js"));
   }
 }
