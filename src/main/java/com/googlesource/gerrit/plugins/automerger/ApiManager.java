63cbd3a4227170c44d5b83268e8d864b2883c131
Stephen Li <stephenli@google.com>
2018-01-29 16:49:15 -0800

    Cross-host support.
    Users are expected to inject their own ApiManager in order to make this
    work. If an ApiManager is not injected, this should behave the same as
    it did before this change.
    
    Change-Id: I6a9c2e6aa39ab0ee650f23da97347274e557a1cf

--- src/main/java/com/googlesource/gerrit/plugins/automerger/ApiManager.java
+++ src/main/java/com/googlesource/gerrit/plugins/automerger/ApiManager.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2016 The Android Open Source Project
+// Copyright (C) 2018 The Android Open Source Project
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,16 +14,24 @@
 
 package com.googlesource.gerrit.plugins.automerger;
 
-/** Class to hold input for a merge for a single source change and destination branch. */
-public class SingleDownstreamMergeInput {
-  public String currentRevision;
-  public int changeNumber;
-  public int patchsetNumber;
-  public String project;
-  public String topic;
-  public String subject;
-  public String downstreamBranch;
-  public boolean doMerge;
-  public String fromCrossHost;
-  public String toCrossHost;
+import com.google.gerrit.extensions.api.GerritApi;
+import com.google.inject.Inject;
+
+/** Interface for sending REST API requests to another Gerrit host. */
+public interface ApiManager {
+  public GerritApi forHostname(String hostname);
+
+  class DefaultApiManager implements ApiManager {
+    protected GerritApi gApi;
+
+    @Inject
+    public DefaultApiManager(GerritApi gApi) {
+      this.gApi = gApi;
+    }
+
+    @Override
+    public GerritApi forHostname(String hostname) {
+      return gApi;
+    }
+  }
 }
