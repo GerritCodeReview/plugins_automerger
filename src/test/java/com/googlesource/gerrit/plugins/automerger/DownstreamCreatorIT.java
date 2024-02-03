44fd7e0e6d2e4f768347226dcb55ddc668bc0b8f
Chris Povirk <cpovirk@google.com>
2024-02-03 18:32:46 -0500

    Remove now-unused import of `Truth8.assertThat`.
    Since Truth
    [1.3.0](https://github.com/google/truth/releases/tag/v1.3.0), the main
    `Truth` class has an `assertThat` overload for `Optional`. Since this
    class already imports `Truth.assertThat`, it uses that for its call to
    `assertThat(getCodeReview(id)).isEmpty()`, so it does not need the
    import of `Truth8.assertThat`.
    
    In fact, importing both `Truth8.assertThat` and `Truth.assertThat` will
    produce a "reference to assertThat is ambiguous" error in an upcoming
    version of Truth.
    
    Change-Id: I80413ed955d871dd3f9e744eca4f467e7c988fac

--- src/test/java/com/googlesource/gerrit/plugins/automerger/DownstreamCreatorIT.java
+++ src/test/java/com/googlesource/gerrit/plugins/automerger/DownstreamCreatorIT.java
@@ -18,7 +18,6 @@ import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.common.truth.OptionalSubject.optionals;
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
-import static com.google.common.truth.Truth8.assertThat;
 import static com.google.gerrit.acceptance.testsuite.project.TestProjectUpdate.allowLabel;
 import static com.google.gerrit.acceptance.testsuite.project.TestProjectUpdate.blockLabel;
 import static com.google.gerrit.acceptance.testsuite.project.TestProjectUpdate.labelPermissionKey;
